f148fad7fb8f010e197e90d301cb6074513ed76a
Dhruv Yadav <dhruv.yadav@vanderbilt.edu>
2016-02-27 00:11:34 -0600

    Still printing out td tags

--- mvp_stats.py
+++ mvp_stats.py
@@ -10,49 +10,36 @@ first_division = soup.find_all("div", {"id":"div_mvp"})
 soup = BeautifulSoup(str(first_division))
 links = soup.find_all("a")
 
+name = []
+
 mvp_name = []
 
 stats = []
 
-# Creating file with "w+" so that we can read and write to it
-# and it also truncates file every time
-holder_file = open("D:\\NBA\\nba_stats\\prettify_holder.txt", "w+")
-
 holder_links = soup.find_all("td")
 holder_soup = BeautifulSoup(str(holder_links))
 
-holder_file.write(holder_soup.prettify().decode("utf-8"))
-
-flag = False
-counter = 0
-
-for line in holder_file:
-    print counter
-    counter = counter + 1
-    print "hello"
-    if (flag == True):
-        stats.append(int(line))
-
-    if "<td align=\"right\">" in line:
-        flag = True
-        print line
-    else:
-        flag = False
-
-holder_file.close()
-
-print stats
-'''
 for link in links:
     if "players" in str(link):
-        print link
-        mvp_name.append(str(link))
+        name.append(str(link))
 
+for name in name:
+    mvp_name.append(name[name.index('html">')+6:name.index('</a')])
 
+for line in holder_links:
+    if "td align=\"right\">" in str(line):
+        stats.append(str(line))
+
+for i in range(len(mvp_name)):
+    if "T" in stats[i]:
+        stats[i] = stats[i][stats[i].index('right">')+7:stats[i].index("T")]
+    else:
+        stats[i] = stats[i][stats[i].index('right">')+7:stats[i].index('</td')]
+
+for i in range(len(mvp_name)):
+    stats[i] = stats[i * 18]
+
+print stats
+print mvp_name
 with open('stats/mvp_names.json', 'w') as outfile:
     json.dump(mvp_name, outfile)
-'''
-
-
-
-# <div class="table_container" id="div_mvp"> 83 lines
