99666ad6dba6d6165120e9d28556226b9e3d1976
rounaksalim95 <rounaksalim@gmail.com>
2016-02-26 22:19:06 -0600

    Trying to extract mvp ranks of players (part 1)

--- mvp_stats.py
+++ mvp_stats.py
@@ -12,6 +12,37 @@ links = soup.find_all("a")
 
 mvp_name = []
 
+stats = []
+
+# Creating file with "w+" so that we can read and write to it
+# and it also truncates file every time
+holder_file = open("D:\\NBA\\nba_stats\\prettify_holder.txt", "w+")
+
+holder_links = soup.find_all("td")
+holder_soup = BeautifulSoup(str(holder_links))
+
+holder_file.write(holder_soup.prettify().decode("utf-8"))
+
+flag = False
+counter = 0
+
+for line in holder_file:
+    print counter
+    counter = counter + 1
+    print "hello"
+    if (flag == True):
+        stats.append(int(line))
+
+    if "<td align=\"right\">" in line:
+        flag = True
+        print line
+    else:
+        flag = False
+
+holder_file.close()
+
+print stats
+'''
 for link in links:
     if "players" in str(link):
         print link
@@ -20,6 +51,8 @@ for link in links:
 
 with open('stats/mvp_names.json', 'w') as outfile:
     json.dump(mvp_name, outfile)
+'''
 
 
-# <div class="table_container" id="div_mvp">
+
+# <div class="table_container" id="div_mvp"> 83 lines
