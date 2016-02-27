f0cdfed5793b017469d508882b3b423cdaffae07
rounaksalim95 <rounaksalim@gmail.com>
2016-02-27 00:43:24 -0600

    Final addition

--- mvp_stats.py
+++ mvp_stats.py
@@ -10,45 +10,10 @@ first_division = soup.find_all("div", {"id":"div_mvp"})
 soup = BeautifulSoup(str(first_division))
 links = soup.find_all("a")
 
-<<<<<<< HEAD
-name = []
-
-mvp_name = []
-=======
 player = []
->>>>>>> 4e83a26d835a9162093f33fdcc0e991518055e8f
 
 mvp_player = []
 
-<<<<<<< HEAD
-holder_links = soup.find_all("td")
-holder_soup = BeautifulSoup(str(holder_links))
-
-for link in links:
-    if "players" in str(link):
-        name.append(str(link))
-
-for name in name:
-    mvp_name.append(name[name.index('html">')+6:name.index('</a')])
-
-for line in holder_links:
-    if "td align=\"right\">" in str(line):
-        stats.append(str(line))
-
-for i in range(len(mvp_name)):
-    if "T" in stats[i]:
-        stats[i] = stats[i][stats[i].index('right">')+7:stats[i].index("T")]
-    else:
-        stats[i] = stats[i][stats[i].index('right">')+7:stats[i].index('</td')]
-
-for i in range(len(mvp_name)):
-    stats[i] = stats[i * 18]
-
-print stats
-print mvp_name
-with open('stats/mvp_names.json', 'w') as outfile:
-    json.dump(mvp_name, outfile)
-=======
 stats = []
 
 holder_links = soup.find_all("td")
