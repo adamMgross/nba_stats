f74afaf00225091be31a50767fed3ea11c70d51e
rounaksalim95 <rounaksalim@gmail.com>
2016-02-28 17:43:16 -0600

    Extracted all ROY stats available since 1984

--- src/scrapers/roy_stats.py
+++ src/scrapers/roy_stats.py
@@ -1,19 +1,19 @@
 '''
-Extracts all rankings for MVP and dumps all data into
+Extracts all rankings for ROY and dumps all data into
 individual json files named by year and type of stat
 '''
 
 from datetime import date
 import basketball_reference
 
-# Year since which MVP rankings were available
-year = 1956
+# Year since which ROY rankings were available
+year = 1984
 
 # Used to get stats till the last year
 current_year = date.today().year
 
 # Type of stat for which you want rankings
-type_of_stat = "div_mvp"
+type_of_stat = "div_roy"
 
 # Dumps all stats for the year into individual json files
 while (year < current_year):
